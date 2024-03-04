2edc90af57abdefcf5e8d5321d2db3e69396e125
OviniP <149691951+OviniP@users.noreply.github.com>
2024-03-04 23:56:53 +0200

    Part 4 - a - done

--- bloglist/tests/helper.test.js
+++ bloglist/tests/helper.test.js
@@ -56,3 +56,89 @@ describe('favorite Blog',() => {
         assert.deepStrictEqual(result,expected)
     })
 })
+
+describe('most blogs', () => {
+    test('list should return the right object', () => {
+        const expected = {
+            author: "Robert C. Martin",
+            blogs: 3
+          }
+        const result = listHelper.mostBlogs(testData.blogs)
+        assert.deepStrictEqual(result, expected)
+    })
+
+    test('returns empty object with no data in the input list', () => {
+        const expected = {}
+        const result = listHelper.mostBlogs([])
+        assert.deepStrictEqual(result, expected)
+    })
+
+    test('returns blogs count 1 with author when one object in the input', () => {
+        const expected = {
+            author: "Michael Chan",
+            blogs: 1
+        }
+        const result = listHelper.mostBlogs([testData.blogs[0]])
+        assert.deepStrictEqual(result, expected)
+    })
+
+    test('returns one object when two authors has the same max blog count', () => {
+        const newBlog = {
+            _id: "5a422aa71b54a676234d17f2",
+            title: "Go To Statement Considered Harmful1",
+            author: "Edsger W. Dijkstra",
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+            likes: 5,
+            __v: 0
+          }
+
+        const expected1 = {
+            author: "Edsger W. Dijkstra",
+            blogs: 3
+        }
+
+        const expected2 = {
+            author: "Robert C. Martin",
+            blogs: 3
+        }
+        const blogData = testData.blogs.slice()
+        blogData.push(newBlog)
+        const result = listHelper.mostBlogs(blogData)
+
+        if (
+            (result.author === expected1.author && result.blogs === expected1.blogs) ||
+            (result.author === expected2.author && result.blogs === expected2.blogs)
+        ) {
+            assert.ok(true);
+        } else {
+            // Test failed
+            assert.fail(`Result (${result.author}, ${result.blogs}) does not match expected1 (${expected1.author}, ${expected1.blogs}) or expected2 (${expected2.author}, ${expected2.blogs})`);
+        }
+    })
+})
+
+describe('Most Likes',() => {
+    test('list should return the right object', () => {
+        const expected = {
+            author: "Edsger W. Dijkstra",
+            likes: 17
+          }
+        const result = listHelper.mostLikes(testData.blogs)
+        assert.deepStrictEqual(result, expected)
+    })
+
+    test('returns empty object with no data in the input list', () => {
+        const expected = {}
+        const result = listHelper.mostLikes([])
+        assert.deepStrictEqual(result, expected)
+    })
+
+    test('returns the likes of given object when one object is in the input', () => {
+        const expected = {
+            author: "Michael Chan",
+            likes: 7
+        }
+        const result = listHelper.mostLikes([testData.blogs[0]])
+        assert.deepStrictEqual(result, expected)
+    })
+})
--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash');
+
 const dummy = (blogs) =>
 {
     return 1
@@ -27,9 +29,52 @@ const favoriteBlog = (blogs) => {
     }
 }
 
+const mostBlogs = (blogs) => {
+    if(!blogs || blogs.length === 0){
+        return {}
+    }
+    const groupedData = lodash.groupBy(blogs, 'author');
+    console.log(groupedData);
+
+    const mapped = lodash.mapValues(groupedData,(o)=>{ return o.length})
+    console.log(mapped);
+
+    const pairWithLargestNumber = lodash.maxBy(lodash.toPairs(mapped), pair => pair[1]);
+    console.log(pairWithLargestNumber);
+
+    const object = lodash.zipObject(['author', 'blogs'], pairWithLargestNumber);
+    console.log(object);
+
+    return object
+}
+
+const mostLikes = (blogs) => {
+    if(!blogs || blogs.length === 0){
+        return {}
+    }
+
+    const groupedData = lodash.groupBy(blogs, 'author');
+    console.log(groupedData)
+
+    const mapped = lodash.mapValues(groupedData,blog => lodash.sumBy(blog,'likes'))
+    //console.log(mapped)
+
+    const pairs = lodash.toPairs(mapped)
+    //console.log(pairs)
+
+    const max =lodash.maxBy(pairs, function(o) { return o[1]; });
+    //console.log(max)
+
+    const object = lodash.zipObject(['author', 'likes'], max);
+    //console.log(object)
+    return object
+}
+
 module.exports = 
 {   
     dummy, 
     totalLikes, 
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
